16ce543cbe1b249a0d566291455da528458ff23e
Richardson Bruno <richardsonbrc.ads@gmail.com>
2022-02-23 23:26:00 -0300

    add main controller

---
+++ src/js/controllers/mainController.js
@@ -0,0 +1,25 @@
+angular.module('app').controller('mainController', function ($scope) {
+  $scope.leads = [
+    {
+      id: 1,
+      name: 'Richardson',
+      email: 'richardsonbrc@gmail.com',
+      created_at: '01/03/2022',
+      telefone: '(77) 998523145',
+    },
+    {
+      id: 2,
+      name: 'Bruno',
+      email: 'richardsonbrc@gmail.com',
+      created_at: '01/03/2022',
+      telefone: '(77) 998523145',
+    },
+    {
+      id: 3,
+      name: 'Carvalho',
+      email: 'richardsonbrc@gmail.com',
+      created_at: '01/03/2022',
+      telefone: '(77) 998523145',
+    },
+  ];
+});
